4f8c612ce311398c5bd3594e5557c2d7c6c497d8
misrliu <liuchao_1017@163.com>
2018-09-20 16:32:29 +0800

    添加描述信息到

--- cloud-config/src/main/java/com/imuk/cloud/config/CloudConfigApplication.java
+++ cloud-config/src/main/java/com/imuk/cloud/config/CloudConfigApplication.java
@@ -3,9 +3,11 @@ package com.imuk.cloud.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * spring cloud server config 配置中心
+ */
 @SpringBootApplication
 public class CloudConfigApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CloudConfigApplication.class, args);
 	}
